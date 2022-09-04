9a193a580d97112e8220bfcb4c23ba222a23b773
Your Name <you@example.com>
2022-09-04 21:59:17 +0600

    code push

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
 const userRoute = require("./Router/User.route")
 const errorHandler = require('./Middleware/ErrorHandler');
 app.use(express.json());
 app.use(errorHandler)
 /************* Api *************/
-app.use("/user", userRoute)
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use("/user", userRoute)
+
 app.get('*', (req, res) => res.send('Node found route'));
 
 
